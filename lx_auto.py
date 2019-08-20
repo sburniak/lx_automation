a6ecb9389933ec6db3af0ae14cfee8e676b06b55
sburniak <sburniak@gmail.com>
2019-08-20 21:25:29 +0200

    Click login to fix and logging will be finished, some basic UTs added

--- lx_auto.py
+++ lx_auto.py
@@ -1,35 +1,57 @@
+import time
+
 from selenium import webdriver
+from selenium.webdriver import ActionChains
 
 
 def get_driver():
     return webdriver.Chrome(r'C:\luxmed-automation\chromedriver.exe')
 
 LX_LOGON = r"https://portalpacjenta.luxmed.pl/PatientPortal/Account/LogOn"
+LX_LOGON1 = r"file:///C:/Users/Seba/Desktop/panelLogowania/Portal%20Pacjenta%20LUX%20MED.html"
 
 
 class LxAccess:
-
     def __init__(self, user, password):
         self._user = user
         self._password = password
         self._browser = None
 
-    def prepare(self, browser):
+    def prepare_browser(self, browser):
         self._browser = browser
-        self._browser.get(LX_LOGON)
+
+    def go_to_page(self, page):
+        self._browser.get(page)
 
     def finish_browser(self):
         self._browser.quit()
 
     def set_username(self):
-        pass
+        login = self._browser.find_element_by_id("Login")
+        login.send_keys(self._user)
+
+    def set_password(self):
+        pswd = self._browser.find_element_by_xpath("//input[@id='Password']")
+        pswd.send_keys(self._password)
 
     def confirm_title_page(self):
         return self._browser.title == "Portal Pacjenta LUX MED"
 
+    def execute_script(self):
+        self._browser.execute_script('document.getElementById("Password").removeAttribute("style")')
+
+    def click_login(self):
+        bttn = self._browser.find_element_by_class_name("submit")
+        bttn.click()
+
 
 if __name__ == "__main__":
-    lxA = LxAccess(1, 2)
-    lxA.prepare(get_driver())
+    lxA = LxAccess('1', '2')
+    lxA.prepare_browser(get_driver())
+    lxA.go_to_page(LX_LOGON1)
     print(lxA.confirm_title_page())
-    lxA.finish_browser()
+    lxA.execute_script()
+    lxA.set_username()
+    # lxA.set_password()
+    lxA.click_login()
+    # lxA.finish_browser()
--- test/test_lxauto.py
+++ test/test_lxauto.py
@@ -1,12 +1,34 @@
 from unittest import TestCase
-from unittest.mock import Mock, MagicMock
-from lx_auto import LxAccess
+from unittest.mock import Mock, MagicMock, patch
+import lx_auto
 
 
 class TestRunBrowser(TestCase):
 
     def setUp(self):
-        self.browser = LxAccess('random', "random")
+        self.lx_a = lx_auto.LxAccess("fake_user", "fake_pass")
+        # self.wb = webdriver
+        # self.wb.Chrome = Mock()
+        self.wb_chrome = patch("lx_auto.webdriver.Chrome")
+        self.patcher = self.wb_chrome.start()
+
+    def tearDown(self):
+        self.patcher.stop()
 
     def test_preparation(self):
-        pass
+        self.lx_a.prepare_browser(self.patcher())
+        self.patcher.assert_called_once()
+
+    def test_go_to_page(self):
+        self.lx_a.prepare_browser(self.patcher)
+        self.lx_a.go_to_page(lx_auto.LX_LOGON)
+        self.patcher.get.assert_called_with(lx_auto.LX_LOGON)
+
+    def test_finish_browser(self):
+        self.lx_a.prepare_browser(self.patcher)
+        self.lx_a.finish_browser()
+        self.patcher.quit.assert_called_once()
+
+    def test_confirm_title_page(self):
+        self.assertTrue(self.lx_a.confirm_title_page())
+
